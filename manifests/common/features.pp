ee8cec5b944334b3f82500faf5429a71cf8fc5bb
Michael Haase <darkstar@gernox.de>
2020-06-06 08:01:46 +0200

    Add icinga2 config

--- manifests/common/features.pp
+++ manifests/common/features.pp
@@ -1,17 +1,7 @@
 # @summary
 #   Main manifest for common Icinga features
 #
-# @param pki_ca_name
-#
-# @param pki_path
-#
-# @param fqdn
-#
-class gernox_icinga2::common::features (
-  String $pki_ca_name,
-  String $pki_path,
-  String $fqdn = $::fqdn,
-) {
+class gernox_icinga2::common::features {
   contain ::icinga2::feature::checker
   contain ::icinga2::feature::mainlog
 
